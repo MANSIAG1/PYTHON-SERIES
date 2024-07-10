ddd8ff32456dbc606231c06c3f4e510d0a2aa416
MANSIAG1 <156298179+MANSIAG1@users.noreply.github.com>
2024-07-11 00:55:03 +0530

    commit2

---
+++ lect2.py
@@ -0,0 +1,27 @@
+#input - input statement is used to accept values(using keyword) from user
+
+# input() # result for input() is always a str
+
+# int(input()) #int
+
+# float(input()) #float
+
+
+# name =input("enter your name :")
+# print("welcome",name)
+
+# age=int(input("enter age"))
+# print(age)
+
+
+# name=input("enter name")
+# age=int(input("enter age"))
+# marks=int(input("enter marks"))
+# print(name,age,marks)
+
+#quest1
+#wap to input side of a square and print its area
+side1=int(input("enter side 1"))
+side2=int(input("enter side 2"))
+area=side1*side2
+print(area)
